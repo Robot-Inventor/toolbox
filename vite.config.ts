87c6bf62513c557284cb38c16f59de0f4d0859a0
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2026-09-15 11:39:43 +0900

    perf: use Rust-based React Compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,12 @@
-import babel from "vite-plugin-babel";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import { reactRouter } from "@react-router/dev/vite";
 
 export default defineConfig({
     plugins: [
-        reactRouter(),
-        babel({
-            babelConfig: {
-                parserOpts: { plugins: ["jsx"] },
-                plugins: [["babel-plugin-react-compiler"]],
-                presets: ["@babel/preset-typescript"]
-            },
-            include: /\.[jt]sx?$/
-        })
+        react({
+            compiler: true
+        }),
+        reactRouter()
     ]
 });
